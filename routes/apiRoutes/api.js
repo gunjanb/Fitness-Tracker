d8e8a2ccf4b3e94935dc6f29324e414976633b1e
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-27 11:02:16 -0700

    routes/apiRoutes/api.js
    updated for getting workout data for front page

--- routes/apiRoutes/api.js
+++ routes/apiRoutes/api.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
+const Workout = require("../../models/workout.js");
+// /api/workouts :all workout sorted on date
+router.get("/workouts", (req, res) => {
+  console.log("here at backend");
 
+  Workout.find({})
+    // .sort({ date: -1 })
+    .then((dbworkoutdata) => {
+      res.status(200).json(dbworkoutdata);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 module.exports = router;
