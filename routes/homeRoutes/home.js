297effbf911acc40d9488a831a6b58fdbe3ab958
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-26 22:53:10 -0700

    updated  routes to displayr home, exercise and stats page

--- routes/homeRoutes/home.js
+++ routes/homeRoutes/home.js
@@ -1,3 +1,18 @@
 const router = require("express").Router();
+const path = require("path");
 
+// get /   : display  the home page
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "../../../public/html/index.html"));
+});
+
+// get /stats  : display stats page
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "../../../public/html/stats.html"));
+});
+
+// get /excercise :display excercise page
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "../../../public/html/exercise.html"));
+});
 module.exports = router;
