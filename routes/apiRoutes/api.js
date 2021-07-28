7e5c45bb9cc5e33f791a3d79a54ddf35cda32f20
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-28 02:18:14 -0700

    updated for creating new workout

--- routes/apiRoutes/api.js
+++ routes/apiRoutes/api.js
@@ -1,16 +1,38 @@
 const router = require("express").Router();
 const Workout = require("../../models/workout.js");
-// /api/workouts :all workout sorted on date
-router.get("/workouts", (req, res) => {
+// GET /api/workouts : get all workout along with totalduration of workout
+router.get("/workouts", async (req, res) => {
   console.log("here at backend");
+  try {
+    const dbworkoutdata = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ]);
 
-  Workout.find({})
-    // .sort({ date: -1 })
-    .then((dbworkoutdata) => {
-      res.status(200).json(dbworkoutdata);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+    console.log("@@@@@@@@@@@@@@@@", dbworkoutdata);
+    res.status(200).json(dbworkoutdata);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
+// POST/api/workouts  : create a new workout
+router.post("/workouts", async ({ body }, res) => {
+  console.log("here at backend for creating new workout");
+  try {
+    console.log(body);
+    const dbworkoutdata = await Workout.create(body);
+
+    console.log("@@@@@@@@@@@@@@@ posting  workout", dbworkoutdata);
+    res.json(dbworkoutdata);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
