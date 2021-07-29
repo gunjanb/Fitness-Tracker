af629f43f7e1bbae26ec636685bc8323ca4ee3f0
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-29 11:44:18 -0700

    updated for dotenv

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
+// modules required
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+// HTTP request logger middleware for node.js
 const logger = require("morgan");
 const routes = require("./routes");
-
+const path = require("path");
+//port
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// middleware
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//serve static files
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//connect to MongoDB
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // routes
-//app.use(require("./routes/api.js"));
 app.use(routes);
 
+//turn on server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
